533c4651fc695563e9e29ebf6c7c28511632fe3b
Sanket <sanket@rajeshpatkar.net>
2015-12-05 10:22:00 +0530

    Stage 3 : adding the request handler while creating a server

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ var http_Module = require("http");
 console.log("Starting index.js");
 
 
-//creating a server 
-var server = http_Module.createServer();
-
-//adding a event handler for the event named "request"
-//every time a request is received by the server .. a request event is generated 
-server.on("request",function(req, res){
+//creating a server
+//while creating the server , the lambda passed is automatically added to request event internally. 
+var server = http_Module.createServer(function(req, res){
 	res.writeHead(200,{"Content-type":"text/plain"});
-	res.end("Just received a request.....");
-})
+	res.end("Response from the server side.\n\n");
+});
 
 //Creating a server is not enough ... developer needs to tell the server on which port should it listen.
 server.listen(3000, function(){
